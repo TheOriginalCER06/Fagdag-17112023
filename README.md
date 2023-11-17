ffd6f843c5065aead85210c420b1788005ef1070
TheOriginalCER06 <69149072+TheOriginalCER06@users.noreply.github.com>
2023-11-17 10:59:01 +0100

    MORE numbers

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## GitHub Pages
 
 You can view the website for this project on [GitHub Pages](https://TheOriginalCER06.github.io/Fagdag-17112023/).
+
+
+## Info
+
+This was a project for a school assignment. That's it.
+Language is Norwegian for anyone wondering.
--- telefon.js
+++ telefon.js
@@ -1,15 +1,12 @@
-fetch('telefon.json')
-  .then(response => response.json())
-.then(data => {
-    const tableBody = document.getElementById('table-body');
-    data.forEach(row => {
-        const tr = document.createElement('tr');
-        const tdNavn = document.createElement('td');
-        const tdTelefonnummer = document.createElement('td');
-        tdNavn.textContent = row.navn;
-        tdTelefonnummer.innerHTML = `<a href="tel:${row.telefonnummer}">${row.telefonnummer}</a>`;
-        tr.appendChild(tdNavn);
-        tr.appendChild(tdTelefonnummer);
-        tableBody.appendChild(tr);
-    });});
-
+fetch('telefon.json').then(response => response.json()).then(data => {
+const tableBody = document.getElementById('table-body');
+data.forEach(row => {
+    const tr = document.createElement('tr');
+    const tdNavn = document.createElement('td');
+    const tdTelefonnummer = document.createElement('td');
+    tdNavn.textContent = row.navn;
+    tdTelefonnummer.innerHTML = `<a href="tel:${row.telefonnummer}">${row.telefonnummer}</a>`;
+    tr.appendChild(tdNavn);
+    tr.appendChild(tdTelefonnummer);
+    tableBody.appendChild(tr);
+});});
