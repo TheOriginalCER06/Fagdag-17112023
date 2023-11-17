a3396c9eeadfc0b9272bf41202381a6f50d82001
TheOriginalCER06 <69149072+TheOriginalCER06@users.noreply.github.com>
2023-11-17 10:34:37 +0100

    color, tlf link

--- telefon.js
+++ telefon.js
@@ -1,15 +1,15 @@
 fetch('telefon.json')
   .then(response => response.json())
-  .then(data => {
+.then(data => {
     const tableBody = document.getElementById('table-body');
     data.forEach(row => {
-      const tr = document.createElement('tr');
-      const tdNavn = document.createElement('td');
-      const tdTelefonnummer = document.createElement('td');
-      tdNavn.textContent = row.navn;
-      tdTelefonnummer.textContent = row.telefonnummer;
-      tr.appendChild(tdNavn);
-      tr.appendChild(tdTelefonnummer);
-      tableBody.appendChild(tr);
-    });
-  });
+        const tr = document.createElement('tr');
+        const tdNavn = document.createElement('td');
+        const tdTelefonnummer = document.createElement('td');
+        tdNavn.textContent = row.navn;
+        tdTelefonnummer.innerHTML = `<a href="tel:${row.telefonnummer}">${row.telefonnummer}</a>`;
+        tr.appendChild(tdNavn);
+        tr.appendChild(tdTelefonnummer);
+        tableBody.appendChild(tr);
+    });});
+
